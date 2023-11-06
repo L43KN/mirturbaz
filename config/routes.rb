b981e0be21c3bd681216bab07f48ae4b23d1da8b
L43KN <ailkushawork@gmail.com>
2023-11-06 21:20:42 +0300

    Создал необходимые model, views, controller и реализовал CRUD для сущности турбаза

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :camps
   resources :cities
   resources :regions
   resources :countries
---
+++ db/migrate/20231106182013_create_camps.rb
@@ -0,0 +1,10 @@
+class CreateCamps < ActiveRecord::Migration[7.1]
+  def change
+    create_table :camps do |t|
+      t.string :name
+      t.references :city, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
